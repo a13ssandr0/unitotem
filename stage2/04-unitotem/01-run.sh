9090bd070159ebaa7e2ffa8191eb8452e50968ce
alessandro <campoloalex@gmail.com>
2022-08-31 15:55:18 +0200

    Splitted manager from main os

--- stage2/04-unitotem/01-run.sh
+++ stage2/04-unitotem/01-run.sh
@@ -1,33 +1,18 @@
 #!/bin/bash -e
 
-# UniTotem manager base environment
-cp -r files/unitotem "${ROOTFS_DIR}/opt/"
-chmod a+x "${ROOTFS_DIR}/opt/unitotem/uniman.py"
-install -v -m 777 -d "${ROOTFS_DIR}/opt/unitotem/static/uploaded"
-on_chroot << EOF
-python3 -m venv /opt/unitotem/env --system-site-packages
-source /opt/unitotem/env/bin/activate
-pip3 install python-crontab flask flask-httpauth requests validators decorator qrcode PyChromeDevTools waitress pymediainfo ruamel.yaml
-deactivate
-EOF
-
-install -v -d "${ROOTFS_DIR}/etc/ssl/unitotem"
-openssl req -x509 -nodes -newkey rsa:2048 -keyout "${ROOTFS_DIR}/etc/ssl/unitotem/key.pem" -out "${ROOTFS_DIR}/etc/ssl/unitotem/crt.pem" -days 365000 -subj "/C=../ST=./L=./O=a13ssandr0/OU=UniTotem/CN=UniTotem/emailAddress=."
-cat ${ROOTFS_DIR}/etc/ssl/unitotem/{crt,key}.pem > "${ROOTFS_DIR}/etc/ssl/unitotem.pem"
-rm -r "${ROOTFS_DIR}/etc/ssl/unitotem"
-chmod 0600 "${ROOTFS_DIR}/etc/ssl/unitotem.pem"
-
-install -v -m 644 files/{start-xorg,unitotem-manager}.service "${ROOTFS_DIR}/etc/systemd/system/"
-on_chroot << EOF
-adduser unitotem || true     # if rerunning the script, it attempts to create the user and finds out it already exists, fails safely
-EOF
-on_chroot << EOF
-systemctl enable start-xorg.service
-systemctl enable unitotem-manager.service
-EOF
-
-install -v files/haproxy.cfg "${ROOTFS_DIR}/etc/haproxy"
+install -v -m 644 files/start-xorg.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -v -m 755 files/xinitrc "${ROOTFS_DIR}/etc/X11/xinit/"
+
+curl -s --compressed -o "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/unitotem-manager.asc" "https://a13ssandr0.github.io/unitotem-manager/KEY.gpg"
+curl -s --compressed -o "${ROOTFS_DIR}/etc/apt/sources.list.d/unitotem-manager.list" "https://a13ssandr0.github.io/unitotem-manager/unitotem-manager.list"
+
+on_chroot << EOF
+# if rerunning the script, it attempts to create the user and finds out it already exists, fails safely
+adduser --disabled-password --gecos "" unitotem || true
+systemctl enable start-xorg.service
+apt-get update
+apt-get -o APT::Acquire::Retries=3 install -y unitotem-manager
+EOF
 
 
 # Enable iptables firewall and allow ports 22, 80, 443 (disable because not working inside chroot)
